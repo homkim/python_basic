d4dde3c94eba7c8bd41a25d22a23df8d52ce90fa
KIM HONGMIN <homkim@naver.com>
2021-06-27 12:23:46 +0900

    commit

---
+++ 02.파이썬기초.py
@@ -0,0 +1,18 @@
+
+print(int(3.5)      ) # 3
+print(2e3           ) # 2000.0
+print(float("1.6")  ) # 1.6
+print(float("inf")  ) # 무한대
+print(float("-inf") ) # -무한대
+print(bool(0)       ) # False. 숫자에서 0만 False임,
+print(bool(-1)      ) # True
+print(bool("False") ) # True
+a = None      # a는 None
+print(a)
+print(a is None)     # a가 None 이므로 True
+
+print("복소수")
+print("===================================")
+v = 2 + 3j
+print(v.real)
+print(v.imag)
